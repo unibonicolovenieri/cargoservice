771b1c0b869c29bc550e5642fb153bb16bae97f1
nicolovenieri <nicolo.venieri2@studio.unibo.it>
2025-09-02 12:47:16 +0200

    Attori nuovi
    Run con nuovi attori ok

--- sprint0/sprint0.pl
+++ sprint0/sprint0.pl
@@ -11,3 +11,13 @@ context(ctx_client, "localhost",  "TCP", "8002").
 context(ctx_test, "localhost",  "TCP", "9000").
  qactor( cargoservice, ctx_cargoservice, "it.unibo.cargoservice.Cargoservice").
  static(cargoservice).
+  qactor( webgui, ctx_cargoservice, "it.unibo.webgui.Webgui").
+ static(webgui).
+  qactor( client, ctx_client, "it.unibo.client.Client").
+ static(client).
+  qactor( sonar, ctx_iodev, "it.unibo.sonar.Sonar").
+ static(sonar).
+  qactor( led, ctx_iodev, "it.unibo.led.Led").
+ static(led).
+  qactor( cargotest, ctx_test, "it.unibo.cargotest.Cargotest").
+ static(cargotest).

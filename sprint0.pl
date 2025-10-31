e37ed14ad91e092d8bbc6ba9f4b57f236ed9850e
nicolovenieri <nicolo.venieri2@studio.unibo.it>
2025-10-31 16:34:59 +0100

    Aggiunto sonar_test
    Simulatore sonar test + Events

---
+++ sprint0.pl
@@ -0,0 +1,21 @@
+%====================================================================================
+% sprint0 description   
+%====================================================================================
+request( load_product, load_product(PID) ).
+reply( load_accepted, load_accepted(SLOT) ).  %%for load_product
+reply( load_refused, load_refused(CAUSA) ).  %%for load_product
+%====================================================================================
+context(ctx_cargoservice, "localhost",  "TCP", "10000").
+context(ctx_iodev, "localhost",  "TCP", "10001").
+context(ctx_test, "localhost",  "TCP", "10002").
+context(ctx_gui, "localhost",  "TCP", "10003").
+ qactor( cargoservice, ctx_cargoservice, "it.unibo.cargoservice.Cargoservice").
+ static(cargoservice).
+  qactor( webgui, ctx_gui, "it.unibo.webgui.Webgui").
+ static(webgui).
+  qactor( sonar, ctx_iodev, "it.unibo.sonar.Sonar").
+ static(sonar).
+  qactor( led, ctx_iodev, "it.unibo.led.Led").
+ static(led).
+  qactor( cargotest, ctx_cargoservice, "it.unibo.cargotest.Cargotest").
+ static(cargotest).

6981b00773acca1c6840198ab35c3d294e196ab6
Jacopo Chergui <jackchergui09@gmail.com>
2026-01-19 15:37:53 +0300

    add: webgui (springboot init)

---
+++ sprint3/webgui/demo/src/test/java/it/unibo/webgui/WebguiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unibo.webgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
